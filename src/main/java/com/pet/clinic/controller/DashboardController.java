319e23f7faa7e745f711578192fef44fd1217475
adioihsan <adioihsan@gmail.com>
2021-05-20 18:39:55 +0700

    Update Menu : Adding left menu to views

--- src/main/java/com/pet/clinic/controller/DashboardController.java
+++ src/main/java/com/pet/clinic/controller/DashboardController.java
@@ -1,4 +1,30 @@
 package com.pet.clinic.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.pet.clinic.App;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 public class DashboardController {
+
+    @FXML
+    private JFXButton btnPatient;
+
+    @FXML
+    void initialize(){
+       btnPatient.setOnAction(new EventHandler<ActionEvent>() {
+           @Override
+           public void handle(ActionEvent event) {
+               try {
+                   App.setRoot("patient");
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+           }
+       });
+
+    }
 }
